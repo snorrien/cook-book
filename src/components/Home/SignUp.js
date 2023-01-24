1308731cb2e0d6f0e62f21446061f37f87d24cbe
Alena Shapovalova <avshapovalova31@gmail.com>
2023-01-24 11:35:10 +0100

    nav old

--- src/components/Home/SignUp.js
+++ src/components/Home/SignUp.js
@@ -6,7 +6,7 @@ import HttpPost from "../Services/HttpPost"
 
 const SignUp = (props) => {
 
-  
+
   const {
     value: enteredName,
     isValid: enteredNameIsValid,
@@ -64,60 +64,60 @@ const SignUp = (props) => {
     ? 'form-control invalid'
     : 'form-control';
 
-  const SendData= () => {
-    let data =  {
+  const SendData = () => {
+    let data = {
       name: enteredName,
-      email: enteredEmail ,
+      email: enteredEmail,
       password: enteredPassword
     }
-  
-  HttpPost(data);
+
+    HttpPost(data);
   }
-  
-  
+
+
   return (
     <div className="HomeContainer">
-    <div className="EnterContainer" onSubmit={FormSignUp}>
-      <form className={nameInputClasses}>
-        <input
-          type='text'
-          id='name'
-          onChange={nameChangeHandler}
-          onBlur={nameBlurHandler}
-          value={enteredName}
-          />
+      <div className="EnterContainer" onSubmit={FormSignUp}>
+        <form className={nameInputClasses}>
           <label for='name'>Имя</label>
-        {nameInputHasError && (<p className="error-text">имя не указано</p>)}
-      </form>
-      <form className={emailInputClasses}>
-        <input type='text'
-          id='email'
-          onChange={emailChangeHandler}
-          onBlur={emailBlurHandler}
-          value={enteredEmail}
+          <input
+            type='text'
+            id='name'
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+            value={enteredName}
           />
+          {nameInputHasError && (<p className="error-text">имя не указано</p>)}
+        </form>
+        <form className={emailInputClasses}>
           <label for='email'>Почта</label>
-          {emailInputHasError && (<p className="error-text">почта не указана</p>)}  
-      </form>
-      <div className={passwordInputClasses}>
-        <label for='pass'>Пароль (6 символов минимум)</label>
-        <div clasName="password-label">
-          <input type='password'
-            id='pass'
-            onChange={passwordChangeHandler}
-            onBlur={passwordBlurHandler}
-            value={enteredPassword} 
-            minlength="6"
+          <input type='text'
+            id='email'
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+            value={enteredEmail}
           />
-          <i class="far fa-eye"></i>
-          {passwordInputHasError && (<p className="error-text">пароль не указан</p>)}
+          {emailInputHasError && (<p className="error-text">почта не указана</p>)}
+        </form>
+        <div className={passwordInputClasses}>
+          <label for='pass'>Пароль (6 символов минимум)</label>
+          <div clasName="password-label">
+            <input type='password'
+              id='pass'
+              onChange={passwordChangeHandler}
+              onBlur={passwordBlurHandler}
+              value={enteredPassword}
+              minlength="6"
+            />
+            <i class="far fa-eye"></i>
+            {passwordInputHasError && (<p className="error-text">пароль не указан</p>)}
+          </div>
+        </div>
+        <div className='form-actions'>
+          <button disabled={!formIsValid} onClick={SendData}>Войти</button>
         </div>
       </div>
-      <div className='form-actions'>
-        <button disabled={!formIsValid} onClick = {SendData}>Войти</button>
-      </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
---
+++ src/components/Root.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar/NavBar";
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <>
+            <NavBar />
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+}
+
+export default Root;
