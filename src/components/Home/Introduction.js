bbcb174c3f76fe4724f7f68812097ccc1073b675
Alena Shapovalova <avshapovalova31@gmail.com>
2023-02-13 12:07:11 +0100

    add greet1

--- src/components/Home/Introduction.js
+++ src/components/Home/Introduction.js
@@ -6,22 +6,24 @@ const Introduction = () => {
             <div className='page-title'>
                 <h1> Твоя поваренная книга</h1>
             </div>
-            <div className="ideas">
+            <div>
                 <div className="card-idea">
-                    <div className="box-idea item--1">
+                    <div className="box-idea">
+                        <img className="img-ideas" src="/img/icons8-greek-salad-100.png" alt="salad" />
                         <span className="text">Сохраняй</span>
                     </div>
-                    <div className="box-idea item--1">
+                    <div className="box-idea">
+                        <img className="img-ideas" src="/img/favicon.ico" alt="egg" />
                         <span className="text">Пробуй</span>
                     </div>
-                    <div className="box-idea item--1">
+                    <div className="box-idea">
+                        <img className="img-ideas" src="/img/icons8-pancake-stack-100.png" alt="pancake" />
                         <span className="text">Делись</span>
                     </div>
                 </div>
-            </div >
-
-            <button id="initial">Вход</button>
-            <button id="initial">Регистрация</button>
+                <button id="initial">Вход</button>
+                <button id="initial">Регистрация</button>
+            </div>
         </div >
 
     )
